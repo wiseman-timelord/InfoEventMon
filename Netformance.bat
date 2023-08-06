a8a84a3cc7379e90ec78cadffc38c68da7438500
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-08-06 08:17:36 +0100

    Name Change

---
+++ Netformance.bat
@@ -0,0 +1,22 @@
+:: Initiation
+@echo off
+Echo.
+Echo.
+
+
+:: Welcome To App
+Echo Ph3ar t3h N3tformanc3r!
+Echo.
+Echo.
+
+
+:: Run PowerShell Command
+@echo on
+powershell -ExecutionPolicy Bypass -File "netformance.ps1"
+@echo off
+Echo.
+Echo.
+
+
+:: Exit Program
+pause
