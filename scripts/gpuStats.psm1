906ef363722caca9b600bca954b8dc56c91c0625
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-12-22 23:43:48 +0000

    All possible variable PWSH 7.4 stat for each theme

--- scripts/gpuStats.psm1
+++ scripts/gpuStats.psm1
@@ -1,3 +1,5 @@
+update the script appropriately...
+
 function Get-GPUStatistic {
     param (
         [string]$Name,
@@ -21,14 +23,7 @@ function Invoke-GPUStats {
         Display-Title  # Display the title
         Write-Host "GPU Statistics:`n"
 
-        # Placeholder for GPU Utilization
-        Get-GPUStatistic -Name "GPU Utilization" -Command { "Not available - requires tool integration" }
-
-        # Placeholder for additional GPU stats
-        Get-GPUStatistic -Name "GPU Temperature" -Command { "Not available - requires tool integration" }
-        Get-GPUStatistic -Name "Memory Usage" -Command { "Not available - requires tool integration" }
-        Get-GPUStatistic -Name "Clock Speeds" -Command { "Not available - requires tool integration" }
-        Get-GPUStatistic -Name "Fan Speed" -Command { "Not available - requires tool integration" }
+        Write-Host "Not possible with only PWSH 7.4"
 
         Start-Sleep -Seconds 3
     }
