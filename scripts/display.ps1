de2da5a27e7acee6626d0ecd4cd8871bc724f631
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2024-02-14 21:04:01 +0000

    Cleaning up menus

--- scripts/display.ps1
+++ scripts/display.ps1
@@ -19,13 +19,11 @@ function Set-ConfigureDisplay {
 
 function Show-MainMenu {
     PrintProgramTitle
-    Write-Host ""
-    Write-Host "                    1. Performance Monitor"
-    Write-Host ""
-    Write-Host "                    2. Device Information"
-    Write-Host ""
-    Write-Host "                    3. Recent Events Report"
-    Write-Host ""
+    Write-Host "`n`n`n`n`n`n`n`n`n"
+    Write-Host "                     1. Performance Monitor`n"
+    Write-Host "                     2. Device Information`n"
+    Write-Host "                     3. Recent Events Report"
+    Write-Host "`n`n`n`n`n`n`n`n`n"
     PrintProgramSeparator
     $choice = Read-Host "Select, Options = 1-3, Exit = X: "
 
@@ -33,7 +31,7 @@ function Show-MainMenu {
         "1" { Show-PerformanceMonitorMenu }
         "2" { Show-DeviceInfoMenu }
         "3" { Show-RecentEventsMenu }
-        "X", "x" { script-FinalizationCode; exit }
+        "x" { script-FinalizationCode; exit }
         default {
             Write-Host "Invalid choice. Please try again."
             Start-Sleep -Seconds 2
@@ -46,8 +44,10 @@ function Show-MainMenu {
 function Show-PerformanceMonitorMenu {
     Clear-Host
     PrintProgramTitle
-    Write-Host "1. Processor Statistics"
-    Write-Host "2. Network Statistics"
+	Write-Host "`n`n`n`n`n`n`n`n`n`n"
+    Write-Host "                    1. Processor Statistics`n"
+    Write-Host "                    2. Network Statistics"
+	Write-Host "`n`n`n`n`n`n`n`n`n`n"
     PrintProgramSeparator
     $choice = Read-Host "Select, Options = 1-2, Back = B: "
 
@@ -72,7 +72,7 @@ function Show-DeviceInfoMenu {
     $choice = Read-Host "Select, Option = B: "
 
     switch ($choice) {
-        "B", "b" { Show-MainMenu }
+        "b" { Show-MainMenu }
         default {
             Write-Host "Invalid choice. Please try again."
             Start-Sleep -Seconds 2
